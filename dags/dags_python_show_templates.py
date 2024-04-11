17a24a74cfb11e441542775ce599d1af946cf676
chansik_oh <ocs1204@moramcnt.com>
2024-04-11 14:28:05 +0900

    template update

--- dags/dags_python_show_templates.py
+++ dags/dags_python_show_templates.py
@@ -7,7 +7,7 @@ from airflow.decorators import task
 with DAG(
     dag_id="dags_python_show_templates",
     schedule="30 9 * * *",
-    start_date=pendulum.datetime(2024, 3, 10, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 4, 9, tz="Asia/Seoul"),
     catchup=True
 ) as dag:
     
