f59c9759f3eb185682d491020507d46dd426a3a1
chansik_oh <ocs1204@moramcnt.com>
2024-04-17 15:58:41 +0900

    240717 update

--- dags/dags_time_sensor_with_async.py
+++ dags/dags_time_sensor_with_async.py
@@ -4,12 +4,12 @@ from airflow.sensors.date_time import DateTimeSensorAsync
 
 with DAG(
     dag_id="dags_time_sensor_with_async",
-    start_date=pendulum.datetime(2023, 5, 1, 0, 0, 0),
-    end_date=pendulum.datetime(2023, 5, 1, 1, 0, 0),
-    schedule="*/10 * * * *",
+    start_date=pendulum.datetime(2024, 4, 17, 0, 0, 0),
+    end_date=pendulum.datetime(2024, 4, 17, 0, 20, 0),
+    schedule="*/2 * * * *",
     catchup=True,
 ) as dag:
     sync_sensor = DateTimeSensorAsync(
         task_id="sync_sensor",
-        target_time="""{{ macros.datetime.utcnow() + macros.timedelta(minutes=5) }}""",
+        target_time="""{{ macros.datetime.utcnow() + macros.timedelta(minutes=1) }}""",
     )
